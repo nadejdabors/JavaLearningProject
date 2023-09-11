d575ecb85f8fb88484ce33ab4d4b4d9f7acb9d60
nadia <nandiny.bors@gmail.com>
2023-09-11 22:19:39 +0300

    Added "Hello World!"

---
+++ src/main/java/com/tekwillacademy/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,7 @@
+package com.tekwillacademy.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
